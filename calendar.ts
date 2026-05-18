9318f8f5ef7319d2447f280038a93ab7982ee4fa
Abiy Hailu <abiyhailu120@gmail.com>
2026-05-18 17:24:43 +0300

    feat:gregorian-calendar-to-julian

--- calendar.ts
+++ calendar.ts
@@ -1,1 +1,1 @@
-class Calendar {}
+const getGeezMonth = () => {};
---
+++ gregorian.ts
@@ -0,0 +1,44 @@
+type DateFormat = { year: number; month: number; date: number };
+
+const ToJulian = ({ year, month, date }: DateFormat) => {
+  /**
+   * This is a shift number that is 1 for jan and feb (1, 2) and 0 for the others
+   * making shifting the months to the end of the year (because we want feb to be
+   * at the end (becuase it is a leap month and we want the leap mess in the end))
+   * easier.
+   */
+  const monthShift = Math.floor((14 - month) / 12); // 1 - for jan and feb
+  const adjustedMonth = month + monthShift * 12 - 3;
+  /**
+   * We add 4800 for safety. To make any negative value positive, choosing 4,800 as
+   * the most likely earliest date we would convert. For BC years. It will be fixed
+   * in the final Julian Days calculation.
+   * Why don't we want negatives? Research
+   * We do not want negative numbers for years because we will do integer division
+   * later for the leap years calculation, and in some languages negative numbers
+   * ingteger division will truncate to 0, leaving Y/4, Y/100, and Y/400 with
+   * negative values 0.
+   */
+  const adjustedYear = year + 4800 - adjustedMonth;
+  const leapYears =
+    Math.floor(adjustedYear / 4) -
+    Math.floor(adjustedYear / 100) +
+    Math.floor(adjustedYear / 400);
+
+  /**
+   * Created by the creators, not derived
+   */
+  const daysUntilMonth = Math.floor((153 * adjustedMonth + 2) / 5);
+
+  /**
+   * The 32045 is actually the calculated number of
+   * OFFSET - NUMBER_OF_DAYS_IN (4800)
+   * The OFFSET is the number of days from the start of Julian Calendar
+   * January 1, 4713 BC to the start of the Gregorian Calendar January 1, 1 AD.
+   */
+  const julianDays =
+    date + daysUntilMonth + leapYears + adjustedYear * 365 - 32045;
+  return julianDays;
+};
+
+console.log(ToJulian({ year: 1970, month: 1, date: 1 }));
