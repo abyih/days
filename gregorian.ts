100b684fcdd35ee5951c20935f72571795cce059
Abiy Hailu <abiyhailu120@gmail.com>
2026-06-01 14:33:48 +0300

    feat:fixed-from-gregorian-and-reverse

--- gregorian.ts
+++ gregorian.ts
@@ -1,16 +1,58 @@
-const fixed_from_gregorian = (year: number, month: number, day: number) => {
+const fixed_year_from_gregorian = (year: number) => {
   const daysUntilYear = 365 * (year - 1);
   const leapYearsCount =
     Math.floor(year / 4) - Math.floor(year / 100) + Math.floor(year / 400);
   const yearDays = daysUntilYear + leapYearsCount;
+  return yearDays;
+};
 
-  const yearIsLeap =
-    (year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0);
+const fixed_month_from_gregorian = (year: number, month: number) => {
+  const yearIsLeap = isYearLeap(year);
   const elapsed30Days = 30 * (month - 1);
   const elapsed31stDays = Math.floor((7 * month - 2) / 12);
   const correction = month <= 2 ? 0 : yearIsLeap ? 1 : 2;
   const monthDays = elapsed30Days + elapsed31stDays - correction;
+  return monthDays;
+};
+
+const isYearLeap = (year: number) =>
+  (year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0);
+
+const fixed_from_gregorian = (year: number, month: number, day: number) => {
+  const yearDays = fixed_year_from_gregorian(year);
+  const monthDays = fixed_month_from_gregorian(year, month);
 
   const fixedDays = yearDays + monthDays + day;
   return fixedDays;
 };
+
+const gregorian_year_from_fixed = (fixed: number) => {
+  const n400 = Math.floor(fixed / 146097);
+  const l400 = fixed % 146097;
+  const n100 = Math.floor(l400 / 36524);
+  const l100 = l400 % 36524;
+  const n4 = Math.floor(l100 / 1461);
+  const l4 = l100 % 1461;
+  const n1 = Math.floor(l4 / 365);
+
+  const year =
+    n400 * 400 + n100 * 100 + n4 * 4 + n1 + (n100 == 4 && n1 == 4 ? 0 : 1);
+  return { year, daysWithinYear: l4 % 365 };
+};
+
+const gregorian_from_fixed = (fixed: number) => {
+  const { year, daysWithinYear } = gregorian_year_from_fixed(fixed);
+  const correction = isYearLeap(year)
+    ? daysWithinYear <= 60
+      ? 0
+      : 1
+    : daysWithinYear <= 60
+      ? 0
+      : 2;
+  const adjustedDays = daysWithinYear + correction;
+  const month = Math.floor((12 * adjustedDays + 373) / 367);
+  const day = daysWithinYear - fixed_month_from_gregorian(year, month);
+  console.log(year, month, day);
+};
+
+gregorian_from_fixed(fixed_from_gregorian(2026, 6, 10));
