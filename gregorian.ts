d415f7e38dc26adfd8e6457993de506ebaaf4776
Abiy Hailu <abiyhailu120@gmail.com>
2026-06-01 12:19:44 +0300

    feat:correct-gregorian-implementation

--- gregorian.ts
+++ gregorian.ts
@@ -1,44 +1,16 @@
-type DateFormat = { year: number; month: number; date: number };
+const fixed_from_gregorian = (year: number, month: number, day: number) => {
+  const daysUntilYear = 365 * (year - 1);
+  const leapYearsCount =
+    Math.floor(year / 4) - Math.floor(year / 100) + Math.floor(year / 400);
+  const yearDays = daysUntilYear + leapYearsCount;
 
-const ToJulian = ({ year, month, date }: DateFormat) => {
-  /**
-   * This is a shift number that is 1 for jan and feb (1, 2) and 0 for the others
-   * making shifting the months to the end of the year (because we want feb to be
-   * at the end (becuase it is a leap month and we want the leap mess in the end))
-   * easier.
-   */
-  const monthShift = Math.floor((14 - month) / 12); // 1 - for jan and feb
-  const adjustedMonth = month + monthShift * 12 - 3;
-  /**
-   * We add 4800 for safety. To make any negative value positive, choosing 4,800 as
-   * the most likely earliest date we would convert. For BC years. It will be fixed
-   * in the final Julian Days calculation.
-   * Why don't we want negatives? Research
-   * We do not want negative numbers for years because we will do integer division
-   * later for the leap years calculation, and in some languages negative numbers
-   * ingteger division will truncate to 0, leaving Y/4, Y/100, and Y/400 with
-   * negative values 0.
-   */
-  const adjustedYear = year + 4800 - adjustedMonth;
-  const leapYears =
-    Math.floor(adjustedYear / 4) -
-    Math.floor(adjustedYear / 100) +
-    Math.floor(adjustedYear / 400);
+  const yearIsLeap =
+    (year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0);
+  const elapsed30Days = 30 * (month - 1);
+  const elapsed31stDays = Math.floor((7 * month - 2) / 12);
+  const correction = month <= 2 ? 0 : yearIsLeap ? 1 : 2;
+  const monthDays = elapsed30Days + elapsed31stDays - correction;
 
-  /**
-   * Created by the creators, not derived
-   */
-  const daysUntilMonth = Math.floor((153 * adjustedMonth + 2) / 5);
-
-  /**
-   * The 32045 is actually the calculated number of
-   * OFFSET - NUMBER_OF_DAYS_IN (4800)
-   * The OFFSET is the number of days from the start of Julian Calendar
-   * January 1, 4713 BC to the start of the Gregorian Calendar January 1, 1 AD.
-   */
-  const julianDays =
-    date + daysUntilMonth + leapYears + adjustedYear * 365 - 32045;
-  return julianDays;
+  const fixedDays = yearDays + monthDays + day;
+  return fixedDays;
 };
-
-console.log(ToJulian({ year: 1970, month: 1, date: 1 }));
